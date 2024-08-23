d2ed23875bfd156bdb6cb96dfb45b697435370b6
berthem <blaztviper22@gmail.com>
2024-08-23 09:40:48 +0800

    new commits

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 function Homepage() {
   return (
     <div>
-      <h1 className="text-3xl">HomePage</h1>
+      <h1 className="text-3xl text-muted-foreground">HomePage</h1>
       <Button variant='outline' size='lg' className="capitalize m-9">
         click me
       </Button>
